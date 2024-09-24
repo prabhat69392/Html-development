4ee31df1c3d18307b0b1d668e2ed08aaf520695e
prabhat69392 <prabhatkumar909758@gmail.com>
2024-09-24 13:16:01 +0530

    change in index.js

--- SQL CONNECT/index.js
+++ SQL CONNECT/index.js
@@ -1,12 +1,37 @@
 const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
 
+const connection =  mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'unlink',
+  password: "12345"
+});
 let getRandomUser =()=> {
- return{
-   userId: faker.string.uuid(),
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-
-  };
+ return[
+   faker.string.uuid(),
+    faker.internet.userName(),
+   faker.internet.email(),
+    faker.internet.password(),
+  ];
 }
-console.log(getRandomUser())
+
+//insert the new data
+let q="insert into user (userid,username,email,password) values (?,?,?,?)"
+let data=[];
+for(let i=0;i<=5;i++){
+  data.push(getRandomUser());
+}
+try{
+connection.query(q,[data],(err,res)=>{
+      if(err) {
+      console.error(err);}
+      else{
+       console.log(res)
+      }
+})
+}
+catch(err){
+   console.log(err);
+}
+connection.end()
